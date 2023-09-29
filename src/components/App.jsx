434a893f4d24963beeb16514d86fa58e01520605
HIMARS-M142 <maksympona@gmail.com>
2023-09-29 13:37:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -11,26 +11,21 @@ const AppContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `;
-
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 export const App = () => {
   const [contacts, setContacts] = useState(null);
   const [filter, setFilter] = useState('');
-  const [defaultContacts, setDefaultContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
   useEffect(() => {
     const localData = localStorage.getItem('contacts');
-    if (localData && JSON.parse(localData).length) {
-      setContacts(JSON.parse(localData));
-    } else {
-      setContacts(defaultContacts);
-    }
+    return localData && JSON.parse(localData).length
+      ? setContacts(JSON.parse(localData))
+      : setContacts(defaultContacts);
   }, []);
-
   useEffect(() => {
     contacts && localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
@@ -40,6 +35,7 @@ export const App = () => {
     if (isRepeat) {
       return alert(`${name} is already in contacts`);
     }
+
     setContacts(prev => {
       return [
         {
@@ -51,15 +47,10 @@ export const App = () => {
       ];
     });
   };
-
   const onButtonDelete = key => {
     setContacts(prev => {
       return prev.filter(({ id }) => id !== key);
     });
-  };
-
-  const onInputValue = ({ target: { value } }) => {
-    setFilter(value);
   };
 
   const filterList = () => {
@@ -69,6 +60,9 @@ export const App = () => {
         name.toLowerCase().includes(filter.toLowerCase())
       )
     );
+  };
+  const onInputValue = ({ target: { value } }) => {
+    setFilter(value);
   };
 
   return (
